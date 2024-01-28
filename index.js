2bd0e650e62f4309d1d6a96fcf10610025c9c3e8
Alex Spector <lxspector@gmail.com>
2024-01-28 13:56:50 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,46 @@
+index.js;
+const yargs = require('yargs');
+const pkg = require('./package.json');
+const { addNote, printNotes, removeNote } = require('./notes.controller');
+
+yargs.version(pkg.version);
+
+yargs.command({
+  command: 'add',
+  describe: 'Add new note to list',
+  builder: {
+    title: {
+      type: 'string',
+      describe: 'Note title',
+      demandOption: true,
+    },
+  },
+  handler({ title }) {
+    addNote(title);
+  },
+});
+
+yargs.command({
+  command: 'list',
+  describe: 'Print all notes',
+  async handler() {
+    printNotes();
+  },
+});
+
+yargs.parse();
+
+yargs.command({
+  command: 'remove',
+  describe: 'Remove note by id',
+  builder: {
+    id: {
+      type: 'string',
+      describe: 'ID of the note to remove',
+      demandOption: true,
+    },
+  },
+  handler({ id }) {
+    removeNote(id);
+  },
+});
